8f13268c66901483064c2844246ef0fb2bdc677a
vincentiusar <vincentiusarnoldfridolin@gmail.com>
2023-03-01 18:36:28 +0700

    Add Constant

---
+++ app/Constants/Table/TableConstant.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Constants\Table;
+
+class TableConstant
+{
+    const OPEN = 'open';
+    const ONGOING = 'ongoing';
+    const CLOSE = 'close';
+    const RESERVED = 'reserved';
+}
